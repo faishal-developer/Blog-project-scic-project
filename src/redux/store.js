c9797a30dcc6260cea86b58469f5ce7a739ffbea
Tonmoy Roy <rxdevs100@gmail.com>
2022-01-01 20:37:46 +0600

    redux setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import blogReducer from './slices/blogSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    blogs: blogReducer,
   },
 });
